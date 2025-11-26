ce21754330c8021a40b46dd9925ed80832071982
GerOBrien021 <gerard.obrien.cork@gmail.com>
2025-11-26 13:05:15 -0500

    add JS for recommendations

--- recommendations.js
+++ recommendations.js
@@ -7,84 +7,80 @@ function showRecommendations() {
     const recommendations = document.getElementById('recommendations-container');
     recommendations.innerHTML = '';
 
-    fetch('travel_recommendation_api.json')
-    .then(response => response.json())
-    .then(data => {
-        /* First, check if the search term is a country name. */
-        const country = data.countries.find(item => item.name.toLowerCase() === term);
-
-        if (country) {
-            console.log('Country found! Here are the cities: ', country.cities);
-
-            /* Create the cards. */
-            country.cities.forEach(dest => {
-                const card = document.createElement('div');
-                card.classList.add('recommendation-card');
-                card.innerHTML = `
-                    <img src="${dest.imageUrl}" alt="${dest.name}" />
-                    <h3>${dest.name}</h3>
-                    <p>${dest.description}</p>
-                    <button>Visit</button>
-                    </div>
-                    `;
-            
-                recommendations.appendChild(card);
-            });
-
-        } else {
-            /* The search term might be a destination type e.g. 'beaches' or 'temples'. */
-            const destinationTypes = Object.getOwnPropertyNames(data);
-            let destinationType = '';
-
-            /* The search term might be a substring of a destination type e.g. 'beach' instead of 'beaches'.*/ 
-            destinationTypes.forEach(item => {
-                if (item.indexOf(term) != -1) {
-                    destinationType = item;
-                }
-            })
-
-            if (destinationType) {
-                console.log('Destination Type found!', destinationType);
-                console.log('Type of destinationType: ', typeof destinationType);
-
+    if (term !== "") {
+        fetch('travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+            /* First, check if the search term is a country name. */
+            const country = data.countries.find(item => item.name.toLowerCase() === term);
+    
+            if (country) {
                 /* Create the cards. */
-                data[destinationType].forEach(dest => {
+                country.cities.forEach(dest => {
                     const card = document.createElement('div');
                     card.classList.add('recommendation-card');
                     card.innerHTML = `
-                      <img src="${dest.imageUrl}" alt="${dest.name}" />
-                      <h3>${dest.name}</h3>
-                      <p>${dest.description}</p>
-                      <button>Visit</button>
-                      </div>
-                      `;
+                        <img src="${dest.imageUrl}" alt="${dest.name}" />
+                        <h3>${dest.name}</h3>
+                        <p>${dest.description}</p>
+                        <button>Visit</button>
+                        </div>
+                        `;
                 
                     recommendations.appendChild(card);
                 });
-
+    
             } else {
-                /* No match for the search term. */
-                console.log('Destination Type not found!', destinationType);
+                /* The search term might be a destination type e.g. 'beaches' or 'temples'. */
+                const destinationTypes = Object.getOwnPropertyNames(data);
+                let destinationType = '';
+    
+                /* The search term might be a substring of a destination type e.g. 'beach' instead of 'beaches'.*/ 
+                destinationTypes.forEach(item => {
+                    if (item.indexOf(term) != -1) {
+                        destinationType = item;
+                    }
+                })
+    
+                if (destinationType) {
+    
+                    /* Create the cards. */
+                    data[destinationType].forEach(dest => {
+                        const card = document.createElement('div');
+                        card.classList.add('recommendation-card');
+                        card.innerHTML = `
+                          <img src="${dest.imageUrl}" alt="${dest.name}" />
+                          <h3>${dest.name}</h3>
+                          <p>${dest.description}</p>
+                          <button>Visit</button>
+                          </div>
+                          `;
+                    
+                        recommendations.appendChild(card);
+                    });
+    
+                } else {
+                    /* No match for the search term. */                
+                    const card = document.createElement('div');
+                    card.classList.add('recommendation-card');
+                    card.innerHTML = `
+                        <img src="./cork_city.webp" alt="Cork City" />
+                        <h3>Sorry, there were no matches!</h3>
+                        <p>We have a Special Offer on trips to Cork in Ireland!</p>
+                        <button>Visit</button>
+                        </div>
+                        `;
                 
-                const card = document.createElement('div');
-                card.classList.add('recommendation-card');
-                card.innerHTML = `
-                    <img src="./cork_city.webp" alt="Cork City" />
-                    <h3>Sorry, there were no matches!</h3>
-                    <p>We have a Special Offer on trips to Cork in Ireland!</p>
-                    <button>Visit</button>
-                    </div>
-                    `;
-            
-                recommendations.appendChild(card);
-
-            }
-        } 
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        recommendations.innerHTML = 'An error occurred while fetching data.';
-    });
+                    recommendations.appendChild(card);
+    
+                }
+            } 
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            recommendations.innerHTML = 'An error occurred while fetching data.';
+        });
+    } 
 
 } /* end of fn showRecommendations */
 
